07fec3940a058121de6dd51d5a5dc02e0f19a95e
Dayer <93074807+dayercher005@users.noreply.github.com>
2024-04-28 15:51:49 +0800

    Add files via upload

---
+++ NS Pay Calculator/NS Pay Calculator/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  NS Pay Calculator
+//
+//  Created by Dayer Cher on 28/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NS Pay Calculator/NS Pay Calculator/NS_Pay_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NS_Pay_CalculatorApp.swift
+//  NS Pay Calculator
+//
+//  Created by Dayer Cher on 28/4/24.
+//
+
+import SwiftUI
+
+@main
+struct NS_Pay_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
